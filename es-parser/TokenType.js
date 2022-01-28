e712fda3b1b74f82b2344bed97057f08c80eab9f
theohuard <92856312+theohuard@users.noreply.github.com>
2022-01-28 13:36:59 +0100

    update token types : plus,moins,multiple ...

--- es-parser/TokenType.js
+++ es-parser/TokenType.js
@@ -1,6 +1,12 @@
 const TokenType = {
 	NUMBER : 'NUMBER',
-	SYMBOL : 'SYMBOL'
+	SYMBOL : 'SYMBOL',
+	PLUS : 'PLUS',
+	MOINS : 'MOINS',
+	MULTIPLE : 'MULTIPLE',
+	PAR_OUVERT : 'PAR_OUVERT',
+	PAR_FERME : 'PAR_FERME',
+	EOF : 'EOF'
 }
 Object.freeze(TokenType)
 
