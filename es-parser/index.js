2d63f1673d8f7ecb19e53c18e30e6499b99973aa
theohuard <92856312+theohuard@users.noreply.github.com>
2022-01-28 13:39:46 +0100

    lexer/parser

--- es-parser/index.js
+++ es-parser/index.js
@@ -4,8 +4,11 @@ const Parser = require('./Parser')
 const src = '2 + ( 7 - 2 ) + 4 * 5'
 //const src = '1234'
 
-const tokens = new Lexer().parse(src)
-const ast = new Parser().parser(tokens)
+const tokens = new Lexer().parse(src);
 
-console.log(src)
 console.log(tokens);
+
+//parser
+const result = new Parser().parser(tokens);
+console.log(src);
+console.log(result)
